272726bd89b258925441220957b788a320e23f6e
Orlando Ding <xiandao.airs@gmail.com>
2012-09-18 16:33:32 +0800

    llv22 - my own tester

---
+++ iphone/QRCodeVerify/QRCodeVerify/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  QRCodeVerify
+//
+//  Created by Ding Orlando on 9/18/12.
+//  Copyright (c) 2012 Ding Orlando. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
